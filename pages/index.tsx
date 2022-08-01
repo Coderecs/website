4d9a79a60118537bf05f7d062da872076662a9fd
Harsh Johar <hpsj3112@gmail.com>
2022-08-01 14:54:56 +0530

    website init

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Coderecs | New age CP</title>
+        <link rel="icon" href="/assets/favicon/favicon.ico" />
+      </Head>
+
+      
+    </div>
+  )
+}
+
+export default Home
