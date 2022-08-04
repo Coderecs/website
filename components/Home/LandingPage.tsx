68a89371ad6272488da7d46c8bd84fd18bad47cb
Harsh Johar <hpsj3112@gmail.com>
2022-08-04 23:50:08 +0530

    landing page

--- components/Home/LandingPage.tsx
+++ components/Home/LandingPage.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { signIn } from "next-auth/react";
+import React from "react";
 
 function LandingPage() {
-  return (
-    <div></div>
-  )
+    return (
+        <div className="h-full w-full relative">
+            <div className="h-2/5 w-4/5 mx-auto flex items-center flex-col justify-center space-y-3">
+                <h1 className="font-poppins text-6xl">
+                    Your <span className="text-primary">Coding</span> Recommeder
+                </h1>
+                <p className="font-poppins text-xl text-center">
+                    Customized recommendations to solve problems on{" "}
+                    <span className="text-primary">codeforces.com</span> based
+                    on previous performance and submissions.
+                </p>
+            </div>
+            <div className="hidden lg:inline-block absolute top-28 right-0">
+                <img
+                    src="/assets/images/landing1.svg"
+                    className="h-[400px] object-cover -z-10"
+                    alt=""
+                />
+            </div>
+            <div className="p-10">
+                <p className="font-poppins">To continue this journey with us.....</p>
+                <button className="bg-primary text-white px-2 py-2 rounded-md" onClick={()=>signIn()}>Sign Up now!</button>
+            </div>
+        </div>
+    );
 }
 
-export default LandingPage
+export default LandingPage;
