e8f533e9357336e5c72cfd003309f70969603285
harsh johar <hpsj3112@gmail.com>
2022-09-06 09:11:10 +0530

    some more config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
     dest: "public",
-    sw: 'service-worker.js'
+    disable: process.env.NODE_ENV==='development',
 });
 
 module.exports = withPWA({
