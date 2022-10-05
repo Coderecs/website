b4de59ffc5d6b03051f729215db3f7681f770ee6
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2022-10-05 14:50:48 +0530

    loader added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider, useSession } from "next-auth/react";
-import Head from "next/head";
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+import Router from 'next/router';
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());  
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
